179399f642450c979526eebea5f9394dc5b791f6
Nick Yu <yynickel@gmail.com>
2019-06-20 18:49:18 +0000

    page dynamically adds option input fields and buttons upon load

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,33 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
+let numOptions = 3;
+
+
+
+
+
+
+
+
+const attachAddOptionButton = () => {
+  const button = $("<button>").attr("id", "add-option").text("Add an Option");
+  button.click(function(event) {
+    event.preventDefault();
+    numOptions++;
+    addOptionInput(numOptions);
+  })
+  $("#submit").after(button);
+}
+
+const addOptionInput = (numOptions) => {
+  const label = $("<label>").attr("for", `option${numOptions}`).text(`Option ${numOptions}`);
+  const inputField = $("<input>").attr("type", "text").attr("name", `option${numOptions}`);
+  $("form").append(label, inputField, $("<br>"));
+}
+
+$(() => {
+  //append 3 option input fields
+  for (let i = 1; i <= 3; i++) {
+    addOptionInput(i);
+  }
+  $("<input>").attr("type", "submit").attr("id", "submit").val("submit").appendTo($("form"));
+  attachAddOptionButton();
+});
