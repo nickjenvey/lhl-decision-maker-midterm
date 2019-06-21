c8f7d6607694a5bc7eaa971f00478241b15e75ff
Nick Yu <yynickel@gmail.com>
2019-06-21 23:49:28 +0000

    added user pages

---
+++ public/scripts/user-page.js
@@ -0,0 +1,48 @@
+const vote = () => {
+  const options = [];
+  $(".sortable .option").each(function(index, value) {
+    options.push({
+      option: $(value).text().trim(),
+      rank: Number(index),
+    });
+  });
+  const url = `/${$("#user").data("id")}`;
+  $.post(url, { options: options }, function(data) {
+    console.log(data);
+  });
+}
+
+const getResult = () => {
+  const url = `/${$("#user").data("id")}/result`;
+  $.getJSON(url, function(data) {
+    data.forEach(element => {
+      $("<div>").addClass("option").text(`${element[0]} has a ranking of ${Math.round(element[1]*100)/100}`).appendTo($("#result-panel"));
+    });
+    $("#result-panel").slideDown();
+  });
+}
+
+let voted = false;
+let resultShown = false;
+
+$(() => {
+
+  $("#vote").click(function() {
+    vote();
+    // $(this).hide();
+  });
+  $("#result").click(function() {
+    if (!resultShown) {
+      getResult();
+      resultShown = true;
+    } else {
+      $("#result-panel .option").remove();
+      $("#result-panel").slideUp();
+      resultShown = false;
+    }
+  });
+  //added sortable
+  $(".sortable").sortable();
+  $(".sortable").disableSelection();
+
+})
